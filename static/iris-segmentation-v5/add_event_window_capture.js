78732e7017e119897a2aa6ed7f8d76b62b5c2a0d
unknown <gosienna@gmail.com>
2021-04-29 01:19:33 +0800

    add iris edge marker and reset buttons

--- static/iris-segmentation-v5/add_event_window_capture.js
+++ static/iris-segmentation-v5/add_event_window_capture.js
@@ -44,13 +44,20 @@ function capture(event){
             document.getElementById("btn_sub").disabled = true;
             add_event_marker_measurement("left_eye")
             count=count+1;
+
           }else if(count==1){
             document.getElementById("btn_add").disabled = false;
             document.getElementById("btn_sub").disabled = false;
             add_event_marker_measurement("marker")
             count=count+1;
+            var button_iris=document.getElementById("button_reset_iris");
+            button_iris.style.visibility="visible"
+            var button_marker=document.getElementById("button_reset_marker");
+            button_marker.style.visibility="visible"
           }else if(count==2){
             count=0;
+            var button_measure=document.getElementById("button_measure");
+            button_measure.style.visibility="hidden"
           }
 
 }
